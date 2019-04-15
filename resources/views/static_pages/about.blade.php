826c08fca2c3909afc27503a22ccafff7666876c
ZziTaiLeo <601537645@qq.com>
2019-04-15 03:07:10 +0000

    基础页面

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+@extends('layouts.default')
+@section('content')
+<h1>关于页</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,8 +11,9 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+*/
